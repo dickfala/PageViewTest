a70c318cdb7cd4baea62d27f09ec77f871516b99
James <yuanyu.chang@coretronic.com>
2015-01-30 14:08:22 +0800

    Initial Commit

---
+++ pageViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pageViewTest
+//
+//  Created by James on 2015/1/30.
+//  Copyright (c) 2015年 coretronic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pageViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pageViewTest
+//
+//  Created by James on 2015/1/30.
+//  Copyright (c) 2015年 coretronic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
